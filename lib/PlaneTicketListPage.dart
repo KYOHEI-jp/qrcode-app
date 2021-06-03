fa4b6ad8c9f7e0e132b96969c7e0e2eae53acb6d
KYOHEI-jp <kingdomhearts510@gmail.com>
2021-06-03 21:37:47 +0900

    新ファイル追加

---
+++ lib/PlaneTicketListPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PlaneTicketListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(title: "Plane ticket"),
+    );
+  }
+}
